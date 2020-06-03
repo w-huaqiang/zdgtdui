289e3c1292ba0e62c09440b1f25d3da35b2d295e
wanghq <845789131@qq.com>
2020-06-03 15:55:21 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ zdgtdui_version=v1.1
 kube_version_list=(1.16.8 1.16.10 1.17.6 1.18.3)
 
 
-get_kube(){
+get_kube_internet(){
 
 kube_version=v$1
 
@@ -27,9 +27,37 @@ rm -rf kubernetes-server-linux-amd64.tar.gz
 
 }
 
+get_kube(){
+
+kube_version=v$1
+
+if [ ! -d roles/master/${kube_version}/files ]
+  then 
+    mkdir -p roles/master/${kube_version}/files
+fi 
+
+if [ ! -d roles/node/${kube_version}/files ]
+  then
+    mkdir -p roles/node/${kube_version}/files
+fi
+wget http://3.1.20.1:8081/kubernetes/${kube_version}/kubernetes-server-linux-amd64.tar.gz
+tar zxvf kubernetes-server-linux-amd64.tar.gz
+cp kubernetes/server/bin/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl} roles/master/files/${kube_version}/
+cp kubernetes/server/bin/{kubelet,kube-proxy} roles/node/files//${kube_version}/
+rm -rf kubernetes
+rm -rf kubernetes-server-linux-amd64.tar.gz
+
+}
+
+
 for i in ${kube_version_list[*]}
   do
-    get_kube $i
+  if [ ! $1 ];then
+      get_kube_internet $i
+    else
+      get_kube $i
+    fi
+
   done
 
 docker build --network=host -t zdgtdui:$zdgtdui_version .
